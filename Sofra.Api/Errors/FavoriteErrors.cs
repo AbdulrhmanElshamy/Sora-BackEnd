3506d9e9e556b5c5edcaf7e47b94a73e0bc6e931
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2024-06-27 03:43:34 +0200

    add favorite error

---
+++ Sofra.Api/Errors/FavoriteErrors.cs
@@ -0,0 +1,10 @@
+﻿using Sofra.Api.Abstractions;
+
+namespace Sofra.Api.Errors
+{
+    public static class FavoriteErrors
+    {
+        public static readonly Error FavoriteNotFound =
+        new("Favorite.NotFound", "No Favorite was found with the given ID", StatusCodes.Status404NotFound);
+    }
+}
