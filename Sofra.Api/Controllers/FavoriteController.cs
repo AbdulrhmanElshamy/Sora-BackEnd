302da19d37cde9a20434de33072b8942db90f9b9
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2024-06-27 03:42:43 +0200

    add favoritecontroler

---
+++ Sofra.Api/Controllers/FavoriteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sofra.Api.Abstractions;
+using Sofra.Api.Contracts.Favorite;
+using Sofra.Api.Services.FavoriteServices;
+
+namespace Sofra.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Customer")]
+    public class FavoriteController(IFavoriteService _favoriteService) : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id, CancellationToken cancellationToken)
+        {
+            var result = await _favoriteService.GetAsync(id, cancellationToken);
+
+            return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> AddAsync([FromBody] FavoriteRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _favoriteService.AddAsync(request, cancellationToken);
+
+            return result.IsSuccess
+                ? CreatedAtAction(nameof(Get), new { id = result.Value.Id }, result.Value)
+                : result.ToProblem();
+        }
+    }
+}
